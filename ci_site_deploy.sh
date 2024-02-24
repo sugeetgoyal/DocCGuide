3496b0ee0e1879136644f69e9ccfb7d57dad7330
Sugeet Goyal <sugeetgoyalbilha@gmail.com>
2024-02-24 10:30:17 +0530

    Updated CI script

--- ci_site_deploy.sh
+++ ci_site_deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 rm -rf docsData
 
@@ -14,36 +14,39 @@ cp -R `find docsData -type d -name "*.doccarchive"` doc_archives
 
 mkdir docs
 
+#echo "Processing Archive: ~/Desktop/DocCGuide.doccarchive"
+#$(xcrun --find docc) process-archive transform-for-static-hosting ~/Desktop/DocCGuide.doccarchive --hosting-base-path DocCGuide --output-path ~/Desktop/Docs
+
 for ARCHIVE in doc_archives/*.doccarchive; do
     cmd() {
         echo "$ARCHIVE" | awk -F'.' '{print $1}' | awk -F'/' '{print tolower($2)}'
     }
     ARCHIVE_NAME="$(cmd)"
     echo "Processing Archive: $ARCHIVE"
-    $(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path DocCGuide/$ARCHIVE_NAME --output-path docs/$ARCHIVE_NAME
+    $(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path DocCGuide --output-path docs
 done
 
 rm -rf docsData
 
-#git config user.name "sugeetgoyal"
-#
-#git config user.email "sugeetgoyalbilha@gmail.com"
-#
-## Change the GitHub URL to your repository
-#git remote set-url origin https://sugeetgoyal:<git_token>@github.com/sugeetgoyal/DocCGuide/
-#
-#git fetch
-#
-#git stash push -u  -- docs doc_archives
-#
-#git checkout feature/doc-hosting
-#
-#rm -rf docs doc_archives
-#
-#git stash apply
-#
-#git add docs doc_archives
-#
-#git commit -m "Updated DocC documentation"
-#
-#git push --set-upstream origin feature/doc-hosting
+git config user.name "sugeetgoyal"
+
+git config user.email "sugeetgoyalbilha@gmail.com"
+
+# Change the GitHub URL to your repository
+git remote set-url origin https://sugeetgoyal:<git_token>@github.com/sugeetgoyal/DocCGuide/
+
+git fetch
+
+git stash push -u  -- docs doc_archives
+
+git checkout feature/doc-hosting
+
+rm -rf docs doc_archives
+
+git stash apply
+
+git add docs doc_archives
+
+git commit -m "Updated DocC documentation"
+
+git push --set-upstream origin feature/doc-hosting
